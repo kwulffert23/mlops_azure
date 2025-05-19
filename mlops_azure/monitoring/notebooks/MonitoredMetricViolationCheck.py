4b14ce9580ff5d1aeeb03bdfec0db98bed9adca4
Kyra Wulffert <kyra.wulffert@databricks.com>
2025-05-19 12:12:15 +0100

    change resources

--- mlops_azure/monitoring/notebooks/MonitoredMetricViolationCheck.py
+++ mlops_azure/monitoring/notebooks/MonitoredMetricViolationCheck.py
@@ -16,7 +16,7 @@
 #
 # Name of the table that is currently being monitored
 dbutils.widgets.text(
-    "table_name_under_monitor", "kw_dev.taxi_fare_model.predictions", label="Full (three-Level) table name"
+    "table_name_under_monitor", "dev.taxi_fare_model.predictions", label="Full (three-Level) table name"
 )
 # Metric to be used for threshold violation check
 dbutils.widgets.text(
